8efc571bb6af3a0056db9029df47811ac417de85
Ayomide Aremu-Cole <121998651+ayomidecole@users.noreply.github.com>
2025-08-01 03:54:29 -0400

    feat: add navigation bar component with basic styling and integrate into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './NavBar';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <h1>Hello, I'm Ayomide</h1>
       <p>Welcome to my portfolio site.</p>
     </div>
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+      <nav style={{ padding: "1rem", borderBottom: "1px solid #ddd" }}>
+        <h2>Ayomide</h2>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
+  
+    
